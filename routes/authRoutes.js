dd257c8543fa43441dde01ff5500f49b520c957a
kaviya <kaviyaramank@gmail.com>
2025-05-01 12:23:48 +0530

    router updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
-const userController = require('../controllers/authController');
+const { register, login,getUsers } = require('../controllers/authController');
+
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/users', authController.getUsers);
+router.get('/users', getUsers);
 
 module.exports = router;
