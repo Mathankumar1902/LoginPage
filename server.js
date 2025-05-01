9c50ef53a0c38079249b7b9f4ecd5aba587d78fc
kaviya <kaviyaramank@gmail.com>
2025-05-01 11:53:09 +0530

    changes router

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api', require('./routes/authRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
